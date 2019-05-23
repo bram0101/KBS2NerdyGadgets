3ba15a13442ba15c23515963a073afee71e12a6a
Bram Stout <me@bramstout.nl>
2019-05-23 11:41:45 +0200

    betere stresstest

--- src/me/team4/nettest/tests/NetworkTestWebStress.java
+++ src/me/team4/nettest/tests/NetworkTestWebStress.java
@@ -22,54 +22,65 @@ SOFTWARE.
 */
 package me.team4.nettest.tests;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import me.team4.nettest.NetworkTest;
 import me.team4.nettest.NetworkUtils;
 import me.team4.nettest.TestResult;
 
 public class NetworkTestWebStress implements NetworkTest {
 
+	private static final int testAmount = 20;
+	private static final int threadAmount = 20;
+	private static AtomicInteger counter = new AtomicInteger(0);
+	
 	@Override
 	public String getName() {
 		return "Web Stresstest";
 	}
 
+	private boolean test(String ip) {
+		counter.set(0);
+		Thread[] t = new Thread[threadAmount];
+		for(int i = 0; i < threadAmount; i++) {
+			t[i] = new Thread(new Runnable() {
+
+				@Override
+				public void run() {
+					for(int i = 0; i < testAmount; i++) {
+						if(NetworkUtils.httpStress(ip, ""))
+							counter.incrementAndGet();
+						else
+							break;
+					}
+				}
+				
+			});
+			t[i].start();
+		}
+		
+		for(int i = 0; i < threadAmount; i++) {
+			try {
+				t[i].join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		return counter.get() == testAmount * threadAmount;
+	}
+	
 	@Override
 	public void run(TestResult result) {
 		String w1_4 = "192.168.10.1"; //ipv4 adres van webserver 1
 		String w2_4 = "192.168.10.2"; //ipv4 adres van webserver 2
-		String ha_4 = "192.168.10.221"; //ipv4 adres van HA proxy op pfSense
+		String ha_4 = "192.168.40.221"; //ipv4 adres van HA proxy op pfSense
 
 		// Http test op de webservers voor de beschikbaarheid van de website en voeg resultaten toe aan log file.
-
-		int testAmount = 10;
 		
-		int resW1 = 0;
-		for(int i = 0; i < testAmount; i++) {
-			if(NetworkUtils.httpStress(w1_4, ""))
-				resW1++;
-			else
-				break;
-		}
-		
-		int resW2 = 0;
-		for(int i = 0; i < testAmount; i++) {
-			if(NetworkUtils.httpStress(w2_4, ""))
-				resW2++;
-			else
-				break;
-		}
-		
-		int resHA = 0;
-		for(int i = 0; i < testAmount; i++) {
-			if(NetworkUtils.httpStress(ha_4, ""))
-				resHA++;
-			else
-				break;
-		}
-		
-		result.addResult("W1", resW1 == testAmount);
-		result.addResult("W2", resW2 == testAmount);
-		result.addResult("HA_proxy", resHA == testAmount);
+		result.addResult("W1", test(w1_4));
+		result.addResult("W2", test(w2_4));
+		result.addResult("HA_proxy", test(ha_4));
 		
 	}
 
